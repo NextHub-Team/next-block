0628cae7ac78da7f0d4c9aaceb2a94c280cb8385
a.taherkhani <a.taherkhani@metapolitan.io>
2024-12-17 07:44:56 +0330

    feat: add event log properties

--- src/event-logs/dto/create-event-log.dto.ts
+++ src/event-logs/dto/create-event-log.dto.ts
@@ -1,3 +1,77 @@
+import { UserLogDto } from '../../user-logs/dto/user-log.dto';
+
+import {
+  // decorators here
+  Type,
+} from 'class-transformer';
+
+import {
+  // decorators here
+  ValidateNested,
+  IsNotEmptyObject,
+  IsString,
+  IsBoolean,
+  IsOptional,
+} from 'class-validator';
+
+import {
+  // decorators here
+  ApiProperty,
+} from '@nestjs/swagger';
+
 export class CreateEventLogDto {
+  @ApiProperty({
+    required: false,
+    type: () => String,
+  })
+  @IsOptional()
+  @IsString()
+  status?: string;
+
+  @ApiProperty({
+    required: false,
+    type: () => Boolean,
+  })
+  @IsOptional()
+  @IsBoolean()
+  processed?: boolean;
+
+  @ApiProperty({
+    required: true,
+    type: () => String,
+  })
+  @IsString()
+  newValue: string;
+
+  @ApiProperty({
+    required: true,
+    type: () => String,
+  })
+  @IsString()
+  oldValue: string;
+
+  @ApiProperty({
+    required: true,
+    type: () => String,
+  })
+  @IsString()
+  property: string;
+
+  @ApiProperty({
+    required: true,
+    type: () => String,
+  })
+  @IsString()
+  entity: string;
+
+  @ApiProperty({
+    required: true,
+    type: () => UserLogDto,
+  })
+  @ValidateNested()
+  @Type(() => UserLogDto)
+  @IsNotEmptyObject()
+  userLog: UserLogDto;
+
   // Don't forget to use the class-validator decorators in the DTO properties.
 }
--- src/event-logs/infrastructure/persistence/relational/entities/event-log.entity.ts
+++ src/event-logs/infrastructure/persistence/relational/entities/event-log.entity.ts
@@ -1,15 +1,62 @@
+import { UserLogEntity } from '../../../../../user-logs/infrastructure/persistence/relational/entities/user-log.entity';
+
 import {
   CreateDateColumn,
   Entity,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
+  JoinColumn,
+  OneToOne,
+  Column,
 } from 'typeorm';
 import { EntityRelationalHelper } from '../../../../../utils/relational-entity-helper';
+import { EventLogStatusEnum } from '../../../../types/event-logs.enum';
 
 @Entity({
   name: 'event_log',
 })
 export class EventLogEntity extends EntityRelationalHelper {
+  @Column({
+    nullable: false,
+    type: String,
+    default: EventLogStatusEnum.Pending,
+  })
+  status?: string;
+
+  @Column({
+    nullable: false,
+    type: Boolean,
+  })
+  processed?: boolean;
+
+  @Column({
+    nullable: false,
+    type: String,
+  })
+  newValue: string;
+
+  @Column({
+    nullable: false,
+    type: String,
+  })
+  oldValue: string;
+
+  @Column({
+    nullable: false,
+    type: String,
+  })
+  property: string;
+
+  @Column({
+    nullable: false,
+    type: String,
+  })
+  entity: string;
+
+  @OneToOne(() => UserLogEntity, { eager: true, nullable: false })
+  @JoinColumn()
+  userLog: UserLogEntity;
+
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
