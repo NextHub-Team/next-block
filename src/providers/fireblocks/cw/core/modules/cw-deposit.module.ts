862244a4000226e11be8d9f27cf7985106fc8ce3
amir taherkhani <amirtaherkhani@outlook.com>
2025-12-11 04:46:52 +0330

    Fix Fireblocks CW deposit dependency injection

--- src/providers/fireblocks/cw/core/modules/cw-deposit.module.ts
+++ src/providers/fireblocks/cw/core/modules/cw-deposit.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { FireblocksCoreModule } from '../fireblocks-core.module';
 import { FireblocksCwService } from '../../fireblocks-cw.service';
 import { CwDepositService } from '../services/cw-deposit.service';
 
 @Module({
-  imports: [FireblocksCoreModule],
+  imports: [forwardRef(() => FireblocksCoreModule)],
   providers: [CwDepositService],
   exports: [CwDepositService],
 })
--- src/providers/fireblocks/cw/core/services/cw-deposit.service.ts
+++ src/providers/fireblocks/cw/core/services/cw-deposit.service.ts
@@ -1,11 +1,14 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { forwardRef, Inject, Injectable, Logger } from '@nestjs/common';
 import { FireblocksCwService } from '../../fireblocks-cw.service';
 
 @Injectable()
 export class CwDepositService {
   private readonly logger = new Logger(CwDepositService.name);
 
-  constructor(private readonly client: FireblocksCwService) {}
+  constructor(
+    @Inject(forwardRef(() => FireblocksCwService))
+    private readonly client: FireblocksCwService,
+  ) {}
 
   async activateAssetInVault(vaultAccountId: string, assetId: string): Promise<void> {
     this.logger.log(`Activate asset ${assetId} in vault ${vaultAccountId}`);
