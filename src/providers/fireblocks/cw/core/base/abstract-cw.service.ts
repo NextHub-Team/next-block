3d23b32282d93f46760168acc1924aa66b123185
amir taherkhani <amirtaherkhani@outlook.com>
2025-12-11 09:43:35 +0330

    Use defaults for Fireblocks config lookups

--- src/providers/fireblocks/cw/core/base/abstract-cw.service.ts
+++ src/providers/fireblocks/cw/core/base/abstract-cw.service.ts
@@ -1,6 +1,7 @@
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AllConfigType } from '../../../../../config/config.type';
+import { FIREBLOCKS_ENV_TYPE } from '../../types/fireblocks-const.type';
 
 export abstract class AbstractCwService {
   protected readonly logger: Logger;
@@ -26,9 +27,9 @@ export abstract class AbstractCwService {
       return;
     }
 
-    const envType =
-      this.configService.get('fireblocks.envType', { infer: true }) ??
-      'unknown';
+    const envType = this.configService.get('fireblocks.envType', FIREBLOCKS_ENV_TYPE, {
+      infer: true,
+    });
     this.debug(`Using environment ${envType}`);
   }
 }
