cfc78ee805c4e3e235f90852b6d19d428cfb84fd
amir taherkhani <amirtaherkhani@outlook.com>
2025-12-15 09:53:14 +0330

    refactor: remove Fireblocks webhook module and related services

--- src/providers/fireblocks/cw/fireblocks-cw.module.ts
+++ src/providers/fireblocks/cw/fireblocks-cw.module.ts
@@ -1,8 +1,7 @@
-import { Global, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { RouterModule } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from '../../../users/users.module';
-import { FireblocksWebhookModule } from './webhook/fireblocks-webhook.module';
 import { FireblocksCwAdminController } from './controllers/fireblocks-cw-admin.controller';
 import { FireblocksCwBaseController } from './fireblocks-cw.controller';
 import { FireblocksCwClientController } from './controllers/fireblocks-cw-client.controller';
@@ -12,12 +11,11 @@ import { FireblocksCwService } from './fireblocks-cw.service';
 import { FireblocksErrorMapper } from './infrastructure/persistence/relational/mappers/fireblocks-error.mapper';
 import { EnableGuard } from '../../../common/guards/service-enabled.guard';
 
-@Global()
+
 @Module({
   imports: [
     ConfigModule,
     UsersModule,
-    FireblocksWebhookModule,
     RouterModule.register([
       {
         path: 'fireblocks/cw',
@@ -39,7 +37,6 @@ import { EnableGuard } from '../../../common/guards/service-enabled.guard';
   ],
   exports: [
     ConfigModule,
-    FireblocksWebhookModule,
     FireblocksCwService,
     FireblocksErrorMapper,
     FireblocksCwAdminService,
