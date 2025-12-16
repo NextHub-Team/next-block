fdbe7afd9ae369cd5aa4cec8cdeb55f4105341da
Parhamya1 <p.yasini@metapolitan.io>
2025-12-16 11:15:34 +0330

    feat: fix wallet address

--- src/address-management/reward-minter/reward-minter.module.ts
+++ src/address-management/reward-minter/reward-minter.module.ts
@@ -3,9 +3,11 @@ import { RewardMinterController } from './reward-minter.controller';
 import { RewardMinterService } from './reward-minter.service';
 import { RewardMinterConsumer } from './infrastructure/messaging/reward-minter.consumer';
 import { AvaxRewardTokenAdapter } from './infrastructure/blockchain/avax-reward-token.adapter';
+import { FireblocksCwModule } from '../../providers/fireblocks/cw/fireblocks-cw.module';
+import { UsersModule } from '../../users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule,FireblocksCwModule],
   controllers: [RewardMinterController, RewardMinterConsumer],
   providers: [RewardMinterService, AvaxRewardTokenAdapter],
   exports: [RewardMinterService, AvaxRewardTokenAdapter],
