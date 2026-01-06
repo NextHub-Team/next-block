a37c7076b2db17489b96e421265d41baba453c70
Parhamya1 <p.yasini@metapolitan.io>
2026-01-06 19:22:23 +0330

    feat: connect to vero rabbitmq

--- src/communication/rabbitMQ/rabbitmq.bindings.ts
+++ src/communication/rabbitMQ/rabbitmq.bindings.ts
@@ -1,7 +1,7 @@
 // RabbitMQ Exchanges (Static)
 export const RMQ_EXCHANGES: Record<string, string> = {
-  orders: 'orders.exchange.topic',
-  inventory: 'inventory.exchange.topic',
+  // orders: 'orders.exchange.topic',
+  // inventory: 'inventory.exchange.topic',
 
    // Address Management: exchange for point-minter
   points: 'points.exchange.topic',
@@ -11,9 +11,9 @@ export const RMQ_EXCHANGES: Record<string, string> = {
 
 // RabbitMQ Queues (Static)
 export const RMQ_QUEUES: Record<string, string> = {
-  orderProcessing: 'order-processing.queue',
-  orderUpdated: 'order-updated.queue',
-  inventoryUpdate: 'inventory-update.queue',
+  // orderProcessing: 'order-processing.queue',
+  // orderUpdated: 'order-updated.queue',
+  // inventoryUpdate: 'inventory-update.queue',
 
     // Address Management: queue for point-minter
   pointMint: 'point-minter.queue',
@@ -21,13 +21,13 @@ export const RMQ_QUEUES: Record<string, string> = {
 
 // RabbitMQ Routing Keys (Static)
 export const RMQ_ROUTING_KEYS: Record<string, Record<string, string>> = {
-  orders: {
-    created: 'order.created',
-    updated: 'order.updated',
-  },
-  inventory: {
-    stockUpdated: 'inventory.stock.updated',
-  },
+  // orders: {
+  //   created: 'order.created',
+  //   updated: 'order.updated',
+  // },
+  // inventory: {
+  //   stockUpdated: 'inventory.stock.updated',
+  // },
 
    // Address Management: routing key for point-minter
   points: {
